fe8a1aff8878ac4027b5340fafdec8c9e6388433
Chris-Wen <whx555666@163.com>
2024-04-26 17:38:53 +0800

    wip: prisma学习，同步及db数据初始化

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server"
 
 export async function GET(request: any, context:any) {
 
-    console.log(context)
+    console.log(111, context)
     // const user = await db.user.findOne({ id })
     return NextResponse.json({data:[], code:200, msg: 'success'}, { status: 200 })
+    // return new NextResponse('success', { status: 200 })
 }
